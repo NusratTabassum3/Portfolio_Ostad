d43230ca58bd088b26868183958a6ede4593d873
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-05-27 02:56:24 +0600

    feature_project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./component/Home";
 import AboutMe from "./component/AboutMe";
 import Experience from "./component/Experience";
+import FeatureProject from "./component/FeatureProject";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Home></Home>
       <AboutMe></AboutMe>
       <Experience></Experience>
+      <FeatureProject></FeatureProject>
     </>
   );
 }
---
+++ src/component/FeatureProject.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function FeatureProject() {
+  return (
+    <div>
+      <section className="bg-[#F2F2F2] py-8 px-8">
+        <div className="max-w-5xl mx-auto min-h-screen ">
+          <div className=" flex justify-center items-center gap-2">
+            <div className="flex grow border-b border-gray-500"></div>
+            <div className="text-2xl">Feature Project </div>
+            <div className="flex grow border-b border-gray-500"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default FeatureProject;
