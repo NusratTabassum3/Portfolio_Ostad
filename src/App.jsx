58199641475ac84d8c03831081b9a8699a668647
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-05-27 21:37:24 +0600

    Fix import path for Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AboutMe from "./component/AboutMe";
 import Experience from "./component/Experience";
 import FeatureProject from "./component/FeatureProject";
 
-import Contact from "./component/contact";
+import Contact from "./component/Contact";
 
 function App() {
   return (
